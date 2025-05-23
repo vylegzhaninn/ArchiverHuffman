d6646686e79fa344930538d278163e8a101fbf2e
Maksim Vylegzhanin <maxvd2017@mail.ru>
2025-05-23 14:02:47 +0700

    Update readwritefunc.h

--- readwritefunc.h
+++ readwritefunc.h
@@ -12,6 +12,6 @@ void WriteBits(uint8_t sym, FILE *output);
 void EncodingSym(FILE *input, FILE *output);
 void writeHuffmanTreeToFile(struct tree_node_t* root, FILE* output);
 struct tree_node_t* readHuffmanTreeFromFile(FILE* input);
-void DecodingSym(FILE *input, FILE *output, struct tree_node_t *root, uint64_t total_chars);
+void DecodingSym(FILE *input, FILE *output, struct tree_node_t *root, int total_chars);
 
 #endif
