f2916fa61ac30e75d16c419b0937f8d121b93c6b
Maksim Vylegzhanin <maxvd2017@mail.ru>
2025-05-23 12:21:13 +0700

    Update writefunc.h

--- writefunc.h
+++ writefunc.h
@@ -1,5 +1,11 @@
-#ifndef WRITEFUNC
-#define WRITEFUNC
+#ifndef READWRITE
+#define READWRITE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "algofhuffman.h"
 
 void buildHuffmanCodes(struct tree_node_t* root, uint8_t *bits, int depth);
 void WriteBits(uint8_t sym, FILE *output);
