1bb9d016c1aafd285cda669c773ec704435e4441
Maksim Vylegzhanin <maxvd2017@mail.ru>
2025-05-23 12:24:04 +0700

    Create readwritefunc.h

---
+++ readwritefunc.h
@@ -0,0 +1,17 @@
+#ifndef READWRITE
+#define READWRITE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "algofhuffman.h"
+
+void buildHuffmanCodes(struct tree_node_t* root, uint8_t *bits, int depth);
+void WriteBits(uint8_t sym, FILE *output);
+void EncodingSym(FILE *input, FILE *output);
+void writeHuffmanTreeToFile(struct tree_node_t* root, FILE* output);
+struct tree_node_t* readHuffmanTreeFromFile(FILE* input);
+void DecodingSym(FILE *input, FILE *output, struct tree_node_t *root, uint64_t total_chars);
+
+#endif
